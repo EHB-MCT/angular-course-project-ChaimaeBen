09decde6f5eafdedac1b9c6b1fdd282d20a264dc
machai <chaimae.benhammou@student.ehb.be>
2022-04-27 20:38:37 +0200

    carousel fix

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -3,14 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.scss']
 })
 export class CarouselComponent implements OnInit {
   images = [
   'https://res.cloudinary.com/dmj6tqnkw/image/upload/v1651014262/GoPlay/wallpaperWitcher1920x1080_cropped_vwno3t.jpg', 
   'https://res.cloudinary.com/dmj6tqnkw/image/upload/v1651014262/GoPlay/wallpaperWitcher1920x1080_cropped_vwno3t.jpg',
   'https://res.cloudinary.com/dmj6tqnkw/image/upload/v1651014262/GoPlay/wallpaperWitcher1920x1080_cropped_vwno3t.jpg',
-].map((n) => n);
+];
 
   constructor() { }
 
