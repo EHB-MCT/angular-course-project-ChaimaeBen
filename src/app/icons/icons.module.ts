6c490d37296b2b8358f83be7aff21f7881670149
ChaimaeBen <chaimae.benhammou@student.ehb.be>
2022-05-21 21:53:14 +0200

    check button done

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatherModule } from 'angular-feather';
-import {User,Search,ChevronRight,Plus,Instagram,Youtube,CornerDownLeft,Share2,HelpCircle,Star,ChevronDown,Filter} from 'angular-feather/icons';
+import {User,Search,ChevronRight,Plus,Instagram,Youtube,CornerDownLeft,Share2,HelpCircle,Star,ChevronDown,Filter,Check} from 'angular-feather/icons';
 
 const icons = {
- User,Search,ChevronRight,Plus,Instagram,Youtube,CornerDownLeft,Share2,HelpCircle,Star,ChevronDown,Filter
+ User,Search,ChevronRight,Plus,Instagram,Youtube,CornerDownLeft,Share2,HelpCircle,Star,ChevronDown,Filter,Check
 };
 @NgModule({
   imports: [
